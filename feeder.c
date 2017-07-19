ed4a9e4453a7d1a7bd044a2851e38f412c9fa658
openbsod <openbsod@protonmail.com>
2017-07-19 18:51:37 +0300

    Code concept

--- feeder.c
+++ feeder.c
@@ -1,7 +1,116 @@
-proof of concept
+// proof of concept
+//tail -fn0 /var/log/nginx/access.log | \
+//while read line; do
+//echo $line | sed -e 's/T[0-9][0-9]:[0-9][0-9]:[0-9][0-9][+][0-9][0-9]:[0-9][0-9]//' -e 's/[+][0-9][0-9]:[0-9][0-9]//' -e 's/./ /62' | \
+//POST 'http://default:@5.101.64.66:8123/?query=INSERT INTO nginx.nginx_access FORMAT JSONEachRow'
+//done
 
-tail -fn0 /var/log/nginx/access.log | \
-while read line; do
-echo $line | sed -e 's/T[0-9][0-9]:[0-9][0-9]:[0-9][0-9][+][0-9][0-9]:[0-9][0-9]//' -e 's/[+][0-9][0-9]:[0-9][0-9]//' -e 's/./ /62' | \
-POST 'http://default:@5.101.64.66:8123/?query=INSERT INTO nginx.nginx_access FORMAT JSONEachRow'
-done
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#define BUFSIZE 32768
+#define BUFSIZE2 65536
+#define OK 1
+#define NO 0
+
+int post(char *header, int lenght, char *ip, int port)
+{
+	if (0 >= snprintf(header, BUFSIZE,
+"POST /?query=INSERT%%20INTO%%20nginx.nginx_streamer%%20FORMAT%%20JSONEachRow HTTP/1.1\n"
+"TE: deflate,gzip;q=0.3\n"
+"Connection: TE, close\n"
+"Host: %s:%d\n"
+"User-Agent: lwp-request/6.03 libwww-perl/6.04\n"
+"Content-Length: %d\n"
+"Content-Type: application/x-www-form-urlencoded\n\n"
+	, ip,port,lenght)) return NO; else return strnlen(header,BUFSIZE); 
+}
+
+int replace(char *src, char *dst)
+{
+	int j,i,q=0;
+	if (!src || !dst) return NO;
+	if (src[0]!='{') return NO;
+	//memcpy(dst,src,BUFSIZE);
+	//{ "requestDate": "2016-11-18T02:12:54+03:00", "requestDateTime": "2016-11-18T02:12:54+03:00"
+	//{ "requestDate": "2016-11-18", "requestDateTime": "2016-11-18 02:12:54"
+	for (i=j=0;i<(BUFSIZE-1)&&src[i];i++) {
+		if (src[i]=='"') q++;
+		if (i==76) {dst[j++]=' '; continue;}
+		if ((i>=28 && i<=42)||(i>=85 && i<=90)) continue;
+		dst[j++]=src[i];
+	}
+	if(q!=36) return NO; //quotes check
+	if(j<BUFSIZE) if(j>80) if(dst[j-2]!='}') return NO; else {dst[j]='\n';dst[j+1]=0; return j+1;}
+	return NO;
+}
+
+int main(int argc, char **argv)
+{
+	char	buf[BUFSIZE], data[BUFSIZE]={0}, header[BUFSIZE]={0};
+	int 	sock=0,
+			n,nn,
+			port=3425;
+	char	ip[256]="127.0.0.1",
+			lpath[]="/var/log/nginx/access.log";
+	FILE *f;
+   struct sockaddr_in addr;
+   struct timeval timeout;      
+   timeout.tv_sec = 5;
+   timeout.tv_usec = 0;
+
+	if (argc==3) {		// j2s 127.0.0.1 2345
+		strncpy(ip,argv[1],256);
+	   port=atoi(argv[2]);
+	}
+
+printf("ip %s \t port %d\n", ip,port);
+
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(port); 
+   inet_pton(AF_INET, ip, &addr.sin_addr);
+
+	if((sock=socket(AF_INET,SOCK_STREAM,0))<0) {perror("socket()");exit(-1);}
+
+	if (setsockopt (sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
+        sizeof(timeout)) < 0)
+        perror("setsockopt failed\n");
+
+	while(1) if((connect(sock,(struct sockaddr*)&addr,sizeof(addr)))>=0) break; else {perror("connect. retry."); sleep(30);}
+
+	for (;;) {
+		if (!(f=fopen(lpath,"r+"))) {puts("cantopenlogfile!");sleep(1);continue;}
+		else puts("-- log (re)opened --");
+		while (1) {
+         printf(".");
+			fgets(buf,sizeof(buf),f);
+			n=replace(buf,data);
+			nn=post(header,n,ip,port);
+			if (!n*nn) continue;
+			printf(">");
+			if (send(sock,header,nn,0)!=nn || send(sock,data,n,0)!=n) {
+fuckagain: 		 
+					puts("--- send error. reconnect ----------");
+					close(sock);
+					sleep(5);
+					if((sock=socket(AF_INET,SOCK_STREAM,0))<0) {perror("socket()");exit(-1);}
+   				if (setsockopt (sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,sizeof(timeout)) < 0)
+        				perror("setsockopt failed\n");
+					if((connect(sock,(struct sockaddr*)&addr,sizeof(addr)))<0) {
+        				perror("--- connect failure. sleep 30 & try again");
+						goto fuckagain;
+					}
+    		} 
+		}
+	}
+
+   if (f) fclose(f);
+   if (sock) close(sock);
+	puts("normal return");
+   return 0;
+}
+
