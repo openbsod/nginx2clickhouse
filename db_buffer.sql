954139f29c100aa8acf4278759ba3e3c9ebb03be
openbsod <openberkeley@icloud.com>
2017-10-19 23:28:46 +0300

    Update db_buffer.sql

--- db_buffer.sql
+++ db_buffer.sql
@@ -1,1 +1,16 @@
-CREATE TABLE nginx.nginx_streamer_buffer AS nginx_streamer ENGINE = Buffer(nginx, nginx_streamer, 16, 10, 100, 10000, 1000000, 10000000, 100000000)
+CREATE TABLE nginx.nginx_streamer_buffer AS nginx_streamer
+ENGINE = Buffer(nginx, nginx_streamer, 16, 10, 100, 10000, 1000000, 10000000, 100000000)
+
+#################  version depended  ######################
+
+CREATE TABLE nginx.nginx_streamer_buffer
+( requestDate Date,
+ requestDateTime DateTime,
+ remote_addr String,
+ body_bytes_sent UInt64,
+ request_time Float32,
+ status String,
+ request String,
+ request_method String,
+ http_user_agent String)
+ENGINE = Buffer(\'nginx\', \'nginx_streamer\', 16, 10, 100, 10000, 1000000, 10000000, 100000000)
